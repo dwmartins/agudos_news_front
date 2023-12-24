ee03d85ca68b1121a53a9cc0e03ef716607087c7
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2023-12-24 14:39:37 -0300

    feat: Configurando rota para a pagina de nova senha

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from '../views/HomePage.vue';
 import CategoryPage from '../views/CategoryPage.vue';
+import NewPasswordPage from '../views/NewPasswordPage.vue';
 
 const routes = [
     {path: '/', name: 'home', component: HomePage},
-    {path: '/categorias', name: 'categorias', component: CategoryPage}
+    {path: '/categorias', name: 'categorias', component: CategoryPage},
+    {path: '/nova-senha', name: 'nova-senha', component: NewPasswordPage}
 ];
 
 const router = createRouter({
---
+++ src/views/NewPasswordPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <AppHeader/>
+        <AppFooter/>
+    </section>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader/AppHeader.vue';
+import AppFooter from '../components/AppFooter/AppFooter.vue';
+
+export default {
+    components: {
+        AppHeader,
+        AppFooter
+    },
+
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+    },
+
+    mounted() {
+        window.scrollTo(0, 0);
+    }
+};
+</script>
+<style scoped>
+</style>
